4f6950021e27fe98bc233a2f0f9e2db83308497a
asatvilker <asatvilker@users.noreply.github.com>
2021-03-12 01:47:27 +0000

    Revert "."
    This reverts commit 8b7e5bb548da55ef8636717596d22440fbcfd0a5.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from "react";
-import {Employee, Admin, RegistryInstance} from './schema/schema';
-import * as objects from './schema/schema'
-import data from './testData/data.json'
-import Holidays from './components/holiday';
-const {REACT_APP_API_KEY}=process.env;
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user:"",
-            
-        }
-        this.updateHolidays = this.updateHolidays.bind(this);
-    }
 
-    
-    
-    componentWillMount(){
-      console.log(REACT_APP_API_KEY)
-      var registry = RegistryInstance
-      
-      for (let index = 0; index < data.users.length; index++) {
-        const element = data.users[index];
-        var employee =new Employee(element.info.adminID,element.info.Fname, element.info.Lname, element.info.salary,element.info.address, element.info.phone,element.info.email, element.info.events, element.info.limit,element.info.balance,element.info.bookings,element.info.requests );
-
-        registry.addEmployee(employee)
-        
-      }
-      
-      var currentEmployee = registry.search("asatvilker");
-      console.log(currentEmployee)
-
-      this.setState({
-        user:currentEmployee,
-          
-        }
-      )
-      
-    }
-    
-    updateHolidays(user) {
-      this.setState({user:user})
-    }
-      
-    
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -62,23 +17,9 @@ class App extends Component {
         >
           Learn React
         </a>
-        <Holidays user={this.state.user} action={this.updateHolidays}/>
-       </header>
-       </div>
-       
-       )
-    }
+      </header>
+    </div>
+  );
 }
 
-
 export default App;
-
-
-
-
-
-
-
-
-
-
